63ad1936681d5d98fdab9b4baf8c60ba626ba4f6
xyrix <xiryx@outlook.com>
2025-10-04 11:09:47 +0100

    added parameters

--- spacewar.py
+++ spacewar.py
@@ -12,6 +12,11 @@ turtle.tracer(1)
 class Sprite(turtle.Turtle):
     def __init__(seld, spriteshape,color,startx,starty):
         turtle.Turtle.__init__(self,shape=spriteshape)
-        self.speedSS
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.fd(0)
+        self.goto(startx,starty)
+        self.speed=1
 
 delay = raw_input("Press enter to finish . >")
