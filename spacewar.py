c1c85a3b279f4ab791f01d9831d21a7fe33cb3fc
xyrix <xiryx@outlook.com>
2025-10-04 11:05:59 +0100

    add class,constructor and defines constructor

--- spacewar.py
+++ spacewar.py
@@ -9,4 +9,9 @@ turtle.ht()
 turtle.setundobuffer(1)
 turtle.tracer(1)
 
+class Sprite(turtle.Turtle):
+    def __init__(seld, spriteshape,color,startx,starty):
+        turtle.Turtle.__init__(self,shape=spriteshape)
+        self.speedSS
+
 delay = raw_input("Press enter to finish . >")
