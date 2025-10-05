d92d982e4cbb29699ffa62be40e2ddeebfd85d9b
xyrix <xiryx@outlook.com>
2025-10-05 12:44:48 +0100

    added gameloop

--- spacewar.py
+++ spacewar.py
@@ -1,12 +1,19 @@
 import os
 import random
 
+#import turtle mode
 import turtle
+#Required by macosx to show the window
 turtle.fd(0)
+#set the animations speed to the maximum
 turtle.speed(0)
+#set the background to black
 turtle.bgcolot("black")
+#hide the default turtle
 turtle.ht()
+#this saves memory
 turtle.setundobuffer(1)
+#this speeds up drawing
 turtle.tracer(1)
 
 class Sprite(turtle.Turtle):
@@ -17,6 +24,13 @@ class Sprite(turtle.Turtle):
         self.color(color)
         self.fd(0)
         self.goto(startx,starty)
-        self.speed=1
+        self.speed= 1
+
+player = Sprite("triangle","white", 0,0)
+
+#game loop
+while True:
+    player.fd(player.speed)
+
 
 delay = raw_input("Press enter to finish . >")
